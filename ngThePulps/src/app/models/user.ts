71e1a3eff0630fb748af2a8d9b77a18b92b1f26e
Michael Harrington <mdharr7@gmail.com>
2023-07-05 18:07:37 +0800

    Create Angular project, configure dev/prod environments

---
+++ ngThePulps/src/app/models/user.ts
@@ -0,0 +1,22 @@
+export class User {
+
+  id: number;
+  username: string;
+  password: string;
+  enabled: boolean;
+  role: string;
+
+  constructor(
+    id: number = 0,
+    username: string = '',
+    password: string = '',
+    enabled: boolean = false,
+    role: string = ''
+  ) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.enabled = enabled;
+    this.role = role;
+  }
+}
