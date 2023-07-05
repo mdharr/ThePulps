03a2dc743aaf6d88506867e825091bb15cc5334b
Michael Harrington <mdharr7@gmail.com>
2023-07-05 14:36:43 +0800

    Stub JPA and Boot/REST projects

---
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/ThePulpsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.thepulps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePulpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThePulpsApplication.class, args);
+	}
+
+}
---
+++ ThePulps/src/test/java/com/skilldistillery/thepulps/ThePulpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.thepulps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThePulpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
