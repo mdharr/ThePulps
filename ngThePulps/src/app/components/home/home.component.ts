d5f7177d8a22636cd9d2eca84903f205659345c1
Michael Harrington <mdharr7@gmail.com>
2023-07-05 20:07:20 +0800

    Stub out home angular home component and set up routing

--- ngThePulps/src/app/components/home/home.component.ts
+++ ngThePulps/src/app/components/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit() {
+    this.tempTestDeleteMeLater(); // DELETE LATER!!!
+  }
+
+  tempTestDeleteMeLater() {
+    this.auth.login('admin', 'wombat1').subscribe({
+      next: (data) => {
+        console.log('Logged in: ');
+        console.log(data);
+
+      },
+      error: (fail) => {
+        console.error('Error authenticating: ');
+        console.error(fail);
+      }
+    });
+  }
 
 }
