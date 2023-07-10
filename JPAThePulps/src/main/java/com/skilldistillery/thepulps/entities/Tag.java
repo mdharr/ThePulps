4c9a7512d60c4fa10edf3a564b1b8aa7d7148898
Michael Harrington <mdharr7@gmail.com>
2023-07-10 20:55:51 +0800

    Stub out Tag entity fields, constructors, getters/setters, hash/equals, and toString

--- JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/Tag.java
+++ JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/Tag.java
@@ -1,8 +1,75 @@
 package com.skilldistillery.thepulps.entities;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.skilldistillery.thepulps.enums.Genre;
 
 @Entity
 public class Tag {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Enumerated(EnumType.STRING)
+	private Genre genre;
 
+	public Tag() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tag(int id, Genre genre) {
+		super();
+		this.id = id;
+		this.genre = genre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Tag [id=").append(id).append(", genre=").append(genre).append("]");
+		return builder.toString();
+	}
+	
 }
