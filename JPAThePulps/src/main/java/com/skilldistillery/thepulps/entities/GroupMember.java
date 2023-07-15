b1f5982a11e79a8fdc3b8e776b085c34d5752148
Michael Harrington <mdharr7@gmail.com>
2023-07-15 17:43:15 +0800

    Stub out group member entity and mappings

--- JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/GroupMember.java
+++ JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/GroupMember.java
@@ -1,10 +1,20 @@
 package com.skilldistillery.thepulps.entities;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "group_member")
@@ -13,5 +23,59 @@ public class GroupMember {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @OneToMany(mappedBy = "group_member_id")
+    private List<GroupMessage> groupMessages;
 
+	public GroupMember() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroupMember(int id, LocalDateTime createdAt, Group group, User user, List<GroupMessage> groupMessages) {
+		super();
+		this.id = id;
+		this.createdAt = createdAt;
+		this.group = group;
+		this.user = user;
+		this.groupMessages = groupMessages;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GroupMember other = (GroupMember) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("GroupMember [id=").append(id).append(", createdAt=").append(createdAt).append(", group=")
+				.append(group).append(", user=").append(user).append("]");
+		return builder.toString();
+	}
+	
 }
