cfb8150d695cd6f90718ccc583c299114ada0d0c
Michael Harrington <mdharr7@gmail.com>
2023-07-15 17:35:39 +0800

    Stub out group entity and mappings to group conversation

--- JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/Group.java
+++ JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/Group.java
@@ -1,9 +1,20 @@
 package com.skilldistillery.thepulps.entities;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 public class Group {
@@ -11,5 +22,112 @@ public class Group {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	private String name;
+	
+	private String description;
+	
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	@MapsId(value = "userId")
+	private User user;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "group_conversation_id")
+	private GroupConversation groupConversation;
 
+	public Group() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Group(int id, String name, String description, LocalDateTime createdAt, User user,
+			GroupConversation groupConversation) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.createdAt = createdAt;
+		this.user = user;
+		this.groupConversation = groupConversation;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public GroupConversation getGroupConversation() {
+		return groupConversation;
+	}
+
+	public void setGroupConversation(GroupConversation groupConversation) {
+		this.groupConversation = groupConversation;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Group other = (Group) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Group [id=").append(id).append(", name=").append(name).append(", description=")
+				.append(description).append(", createdAt=").append(createdAt).append(", user=").append(user)
+				.append(", groupConversation=").append(groupConversation).append("]");
+		return builder.toString();
+	}
+	
 }
