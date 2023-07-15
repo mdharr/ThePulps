737d07270ad8a3793de9ebc444cb8ba6b5dc0770
Michael Harrington <mdharr7@gmail.com>
2023-07-15 21:28:41 +0800

    Stub out user conversation entity in jpa project

--- JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/UserConversation.java
+++ JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/UserConversation.java
@@ -1,5 +1,128 @@
 package com.skilldistillery.thepulps.entities;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name = "user_conversation")
 public class UserConversation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@JsonManagedReference
+	@OneToMany(mappedBy = "userConversation", cascade = CascadeType.ALL)
+	private List<PrivateMessage> privateMessages = new ArrayList<>();
+
+	public UserConversation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserConversation(int id, LocalDateTime createdAt, LocalDateTime updatedAt, User user,
+			List<PrivateMessage> privateMessages) {
+		super();
+		this.id = id;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.user = user;
+		this.privateMessages = privateMessages;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<PrivateMessage> getPrivateMessages() {
+		return privateMessages;
+	}
+
+	public void setPrivateMessages(List<PrivateMessage> privateMessages) {
+		this.privateMessages = privateMessages;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserConversation other = (UserConversation) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("UserConversation [id=").append(id).append(", createdAt=").append(createdAt)
+				.append(", updatedAt=").append(updatedAt).append(", user=").append(user).append("]");
+		return builder.toString();
+	}
 
 }
