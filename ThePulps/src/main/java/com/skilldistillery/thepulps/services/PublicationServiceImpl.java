69798092ecc8f4e4c5f942637b86910b02eb7eed
Michael Harrington <mdharr7@gmail.com>
2023-07-16 20:59:42 +0800

    Add method logic for CRUD operations in publication service implementation

--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/PublicationServiceImpl.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/PublicationServiceImpl.java
@@ -2,38 +2,52 @@ package com.skilldistillery.thepulps.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.skilldistillery.thepulps.entities.Publication;
+import com.skilldistillery.thepulps.repositories.PublicationRepository;
 
 public class PublicationServiceImpl implements PublicationService {
+	
+	@Autowired
+	private PublicationRepository publicationRepo;
 
 	@Override
 	public Publication createPublication(Publication publication) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        return publicationRepo.save(publication);
+    }
 
 	@Override
-	public Publication getPublicationById(int publicationId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Publication getPublicationById(int publicationId) {
+        return publicationRepo.findById(publicationId).orElse(null);
+    }
 
 	@Override
-	public List<Publication> getAllPublications() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public List<Publication> getAllPublications() {
+        return publicationRepo.findAll();
+    }
 
 	@Override
-	public Publication updatePublication(int publicationId, Publication publication) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Publication updatePublication(int publicationId, Publication publication) {
+        Publication existingPublication = publicationRepo.findById(publicationId).orElse(null);
+        if (existingPublication != null) {
+            existingPublication.setName(publication.getName());
+            existingPublication.setThumbnailUrl(publication.getThumbnailUrl());
+            existingPublication.setImageUrl(publication.getImageUrl());
+            // Update other properties as needed
+            return publicationRepo.save(existingPublication);
+        }
+        return null;
+    }
 
 	@Override
-	public boolean deletePublication(int publicationId) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public boolean deletePublication(int publicationId) {
+        Publication publication = publicationRepo.findById(publicationId).orElse(null);
+        if (publication != null) {
+            publicationRepo.delete(publication);
+            return true;
+        }
+        return false;
+    }
 
 }
