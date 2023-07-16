2bdf9bb8c39fa39a594867fd6837878b2290af44
Michael Harrington <mdharr7@gmail.com>
2023-07-16 20:57:15 +0800

    Add unimplemented methods from publication service to publication service implementation

--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/PublicationServiceImpl.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/PublicationServiceImpl.java
@@ -1,5 +1,39 @@
 package com.skilldistillery.thepulps.services;
 
+import java.util.List;
+
+import com.skilldistillery.thepulps.entities.Publication;
+
 public class PublicationServiceImpl implements PublicationService {
+
+	@Override
+	public Publication createPublication(Publication publication) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Publication getPublicationById(int publicationId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Publication> getAllPublications() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Publication updatePublication(int publicationId, Publication publication) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deletePublication(int publicationId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
