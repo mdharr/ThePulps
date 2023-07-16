bc738078781805f0d7498eeac6d7b1560e17607b
Michael Harrington <mdharr7@gmail.com>
2023-07-16 22:13:39 +0800

    Add method to author service and service implementation for retrieving a list of all authors

--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/AuthorService.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/AuthorService.java
@@ -1,5 +1,21 @@
 package com.skilldistillery.thepulps.services;
 
+import java.util.List;
+
+import com.skilldistillery.thepulps.entities.Author;
+
 public interface AuthorService {
+
+//	
+//	public Publication createPublication(Publication publication);
+//
+//    public Publication getPublicationById(int publicationId);
+
+    public List<Author> getAllAuthors();
+//
+//    public Publication updatePublication(int publicationId, Publication publication);
+//
+//    public boolean deletePublication(int publicationId);
+
 
 }
--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/AuthorServiceImpl.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/AuthorServiceImpl.java
@@ -1,8 +1,22 @@
 package com.skilldistillery.thepulps.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.thepulps.entities.Author;
+import com.skilldistillery.thepulps.repositories.AuthorRepository;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
+	
+	@Autowired
+	private AuthorRepository authorRepo;
+
+	@Override
+	public List<Author> getAllAuthors() {
+		return authorRepo.findAll();
+	}
 
 }
