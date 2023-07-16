63a38402aa1b7386a466688e6ea6d2f6130fe4a4
Michael Harrington <mdharr7@gmail.com>
2023-07-16 20:56:41 +0800

    Add necessary methods to perform CRUD operations in publication service

--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/PublicationService.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/PublicationService.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.thepulps.services;
 
-public interface PublicationService {
+import java.util.List;
 
+import com.skilldistillery.thepulps.entities.Publication;
+
+public interface PublicationService {
+	
+	public Publication createPublication(Publication publication);
+
+    public Publication getPublicationById(int publicationId);
+
+    public List<Publication> getAllPublications();
+
+    public Publication updatePublication(int publicationId, Publication publication);
+
+    public boolean deletePublication(int publicationId);
+	
 }
