eb5fa5027916ec36c399805ed30d6622cf856bb7
Michael Harrington <mdharr7@gmail.com>
2023-07-16 15:13:51 +0800

    Stub out tests for magazine html entity and test mapping to magazine entity

--- JPAThePulps/src/test/java/com/skilldistillery/thepulps/entities/MagazineHtmlTest.java
+++ JPAThePulps/src/test/java/com/skilldistillery/thepulps/entities/MagazineHtmlTest.java
@@ -2,6 +2,10 @@ package com.skilldistillery.thepulps.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,25 +14,44 @@ import org.junit.jupiter.api.Test;
 
 class MagazineHtmlTest {
 
+	private static EntityManagerFactory emf;
+	
+	private EntityManager em;
+	
+	private MagazineHtml magazineHtml;
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAThePulps");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		magazineHtml = em.find(MagazineHtml.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		magazineHtml = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_MagazineHtml_entity_mapping() {
+		assertNotNull(magazineHtml);
+		assertEquals("https://www.gutenberg.org/files/68957/68957-h/68957-h.htm", magazineHtml.getFileUrl());
+	}
+	
+	@Test
+	void test_MagazineHtml_Magazine_one_to_one_mapping() {
+		assertNotNull(magazineHtml);
+		assertEquals("Weird Tales, August 1928", magazineHtml.getMagazine().getName());
 	}
 
 }
