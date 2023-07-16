444ed40bf86b2b8d61154bc408d0efb39c75b9e6
Michael Harrington <mdharr7@gmail.com>
2023-07-16 14:55:38 +0800

    Stub out story anchor and embeddable id classes

--- JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/StoryAnchor.java
+++ JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/StoryAnchor.java
@@ -1,5 +1,43 @@
 package com.skilldistillery.thepulps.entities;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "story_anchor")
 public class StoryAnchor {
+	
+	@EmbeddedId
+	private StoryAnchorId id;
+	
+	@Column(name = "anchor_tag")
+	private String anchorTag;
+
+	@ManyToOne
+	@JoinColumn(name = "story_id")
+	@MapsId(value = "storyId")
+	private Story story;
+	
+	@ManyToOne
+	@JoinColumn(name = "magazine_html_id")
+	@MapsId(value = "magazineHtmlId")
+	private MagazineHtml magazineHtml;
+
+	public StoryAnchor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public StoryAnchor(StoryAnchorId id, Story story, MagazineHtml magazineHtml) {
+		super();
+		this.id = new StoryAnchorId(story.getId(), magazineHtml.getId);
+		this.story = story;
+		this.magazineHtml = magazineHtml;
+	}
 
 }
--- JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/StoryAnchorId.java
+++ JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/StoryAnchorId.java
@@ -1,5 +1,78 @@
 package com.skilldistillery.thepulps.entities;
 
-public class StoryAnchorId {
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "story_author")
+public class StoryAnchorId implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "story_id")
+	private int storyId;
+	
+	@Column(name = "magazine_html_id")
+	private int magazineHtmlId;
+
+	public StoryAnchorId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StoryAnchorId(int storyId, int magazineHtmlId) {
+		super();
+		this.storyId = storyId;
+		this.magazineHtmlId = magazineHtmlId;
+	}
+
+	public int getStoryId() {
+		return storyId;
+	}
+
+	public void setStoryId(int storyId) {
+		this.storyId = storyId;
+	}
+
+	public int getMagazineHtmlId() {
+		return magazineHtmlId;
+	}
+
+	public void setMagazineHtmlId(int magazineHtmlId) {
+		this.magazineHtmlId = magazineHtmlId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(magazineHtmlId, storyId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StoryAnchorId other = (StoryAnchorId) obj;
+		return magazineHtmlId == other.magazineHtmlId && storyId == other.storyId;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("StoryAnchorId [storyId=").append(storyId).append(", magazineHtmlId=").append(magazineHtmlId)
+				.append("]");
+		return builder.toString();
+	}
 
 }
