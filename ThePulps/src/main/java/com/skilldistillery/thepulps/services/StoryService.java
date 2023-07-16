9bbd734e4063b3c9ac6613e1716ee805ec96ac73
Michael Harrington <mdharr7@gmail.com>
2023-07-16 22:08:34 +0800

    Add method to story service and service impl for retrieving all stories

--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/StoryService.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/StoryService.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.thepulps.services;
 
+import java.util.List;
+
+import com.skilldistillery.thepulps.entities.Story;
+
 public interface StoryService {
+//	
+//	public Publication createPublication(Publication publication);
+//
+//    public Publication getPublicationById(int publicationId);
+
+    public List<Story> getAllStories();
+//
+//    public Publication updatePublication(int publicationId, Publication publication);
+//
+//    public boolean deletePublication(int publicationId);
 
 }
--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/StoryServiceImpl.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/StoryServiceImpl.java
@@ -1,8 +1,22 @@
 package com.skilldistillery.thepulps.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.thepulps.entities.Story;
+import com.skilldistillery.thepulps.repositories.StoryRepository;
 
 @Service
 public class StoryServiceImpl implements StoryService {
+	
+	@Autowired
+	private StoryRepository storyRepo;
+
+	@Override
+	public List<Story> getAllStories() {
+		return storyRepo.findAll();
+	}
 
 }
