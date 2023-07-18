ead1b9c381a720cf3c139a1c814d4a616deb717d
Michael Harrington <mdharr7@gmail.com>
2023-07-18 16:44:01 +0800

    Create repository for magazine tag

---
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/repositories/MagazineTagRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.thepulps.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.thepulps.entities.MagazineTag;
+
+public interface MagazineTagRepository extends JpaRepository<MagazineTag, Integer> {
+
+}
