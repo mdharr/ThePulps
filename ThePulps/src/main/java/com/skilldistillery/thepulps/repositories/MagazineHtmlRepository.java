bd3ef6efa8e0329a66e453d8c5d4bcb107565ee1
Michael Harrington <mdharr7@gmail.com>
2023-07-18 11:53:02 +0800

    Create repository for magazine html

---
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/repositories/MagazineHtmlRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.thepulps.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.thepulps.entities.MagazineHtml;
+
+public interface MagazineHtmlRepository extends JpaRepository<MagazineHtml, Integer> {
+
+}
