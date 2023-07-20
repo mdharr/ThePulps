548acd369f28ab2f71bb85611e968967b049b08b
Michael Harrington <mdharr7@gmail.com>
2023-07-20 11:33:29 +0800

    Implement methods in user follower repository for finding all by followed id and find all by follower id

--- ThePulps/src/main/java/com/skilldistillery/thepulps/repositories/UserFollowerRepository.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/repositories/UserFollowerRepository.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.thepulps.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.thepulps.entities.UserFollower;
 
 public interface UserFollowerRepository extends JpaRepository<UserFollower, Integer> {
+	
+	List<UserFollower> findAllByFollowed_Id(int userId);
+	
+	List<UserFollower> findAllByFollower_Id(int userId);
 
 }
