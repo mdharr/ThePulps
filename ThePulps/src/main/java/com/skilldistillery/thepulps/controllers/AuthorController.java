0ea96b069837a4f578c0cef6a6c9b0e7b735e612
Michael Harrington <mdharr7@gmail.com>
2023-07-22 12:47:18 +0800

    Add rest api endpoint to controller for retrieving author by id. Successfully tested with Postman.

--- ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/AuthorController.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/AuthorController.java
@@ -2,9 +2,12 @@ package com.skilldistillery.thepulps.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,14 @@ public class AuthorController {
     @GetMapping("authors")
     public List<Author> getAllAuthors() {
         return authorService.getAllAuthors();
+    }
+    
+    @GetMapping("authors/{aid}")
+    public Author getAuthorById(@PathVariable("aid") int authorId, HttpServletResponse res) {
+    	if(authorService.getAuthorById(authorId) == null) {
+    		res.setStatus(404);
+    	}
+    	return authorService.getAuthorById(authorId);
     }
 
 }
