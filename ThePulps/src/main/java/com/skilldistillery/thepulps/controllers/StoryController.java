7a8ab271382b1bdb4239fa3eb8b43bc290a99ee1
Michael Harrington <mdharr7@gmail.com>
2023-07-22 12:39:09 +0800

    Add rest api endpoint to controller for retrieving story by id. Successfully tested with Postman.

--- ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/StoryController.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/StoryController.java
@@ -2,9 +2,12 @@ package com.skilldistillery.thepulps.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,14 @@ public class StoryController {
     @GetMapping("stories")
     public List<Story> getAllStories() {
         return storyService.getAllStories();
+    }
+    
+    @GetMapping("stories/{sid}")
+    public Story getStoryById(@PathVariable("sid") int storyId, HttpServletResponse res) {
+    	if(storyService.getStoryById(storyId) == null) {
+    		res.setStatus(404);
+    	}
+    	return storyService.getStoryById(storyId);
     }
 
 }
