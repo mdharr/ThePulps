a58275d1cf624c769334ddba852ec5f8fd4ba769
Michael Harrington <mdharr7@gmail.com>
2023-07-22 12:52:40 +0800

    Add rest api endpoint to controller for retrieving user by id. Successfully tested with Postman.

--- ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/UserController.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/UserController.java
@@ -2,9 +2,12 @@ package com.skilldistillery.thepulps.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,14 @@ public class UserController {
     @GetMapping("users")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+    
+    @GetMapping("users/{uid}")
+    public User getUserById(@PathVariable("uid") int userId, HttpServletResponse res) {
+    	if(userService.getUserById(userId) == null) {
+    		res.setStatus(404);
+    	}
+    	return userService.getUserById(userId);
     }
 
 }
