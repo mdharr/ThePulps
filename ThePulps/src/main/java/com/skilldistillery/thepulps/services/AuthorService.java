7b5794efde2858877117bf0c31ad54b81ed93b4e
Michael Harrington <mdharr7@gmail.com>
2023-07-22 12:43:57 +0800

    Add service method for retrieving author by id

--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/AuthorService.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/AuthorService.java
@@ -6,15 +6,14 @@ import com.skilldistillery.thepulps.entities.Author;
 
 public interface AuthorService {
 
-//	
 //	public Publication createPublication(Publication publication);
-//
-//    public Publication getPublicationById(int publicationId);
+
+    public Author getAuthorById(int authorId);
 
     public List<Author> getAllAuthors();
-//
+
 //    public Publication updatePublication(int publicationId, Publication publication);
-//
+
 //    public boolean deletePublication(int publicationId);
 
 
--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/AuthorServiceImpl.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/AuthorServiceImpl.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.thepulps.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,16 @@ public class AuthorServiceImpl implements AuthorService {
 	@Override
 	public List<Author> getAllAuthors() {
 		return authorRepo.findAll();
+	}
+
+	@Override
+	public Author getAuthorById(int authorId) {
+		Author author = null;
+		Optional<Author> authorOpt = authorRepo.findById(authorId);
+		if(authorOpt.isPresent()) {
+			author = authorOpt.get();
+		}
+		return author;
 	}
 
 }
