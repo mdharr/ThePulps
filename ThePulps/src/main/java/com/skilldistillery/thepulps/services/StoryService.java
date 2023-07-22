ce7abf5021fdcff21f3154f613fe9beda3e618ae
Michael Harrington <mdharr7@gmail.com>
2023-07-22 12:30:35 +0800

    Add service method for retrieving stories by id

--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/StoryService.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/StoryService.java
@@ -5,15 +5,15 @@ import java.util.List;
 import com.skilldistillery.thepulps.entities.Story;
 
 public interface StoryService {
-//	
+	
 //	public Publication createPublication(Publication publication);
-//
-//    public Publication getPublicationById(int publicationId);
+
+    public Story getStoryById(int storyId);
 
     public List<Story> getAllStories();
-//
+
 //    public Publication updatePublication(int publicationId, Publication publication);
-//
+
 //    public boolean deletePublication(int publicationId);
 
 }
--- ThePulps/src/main/java/com/skilldistillery/thepulps/services/StoryServiceImpl.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/StoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.thepulps.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,17 @@ public class StoryServiceImpl implements StoryService {
 	@Override
 	public List<Story> getAllStories() {
 		return storyRepo.findAll();
+	}
+
+	@Override
+	public Story getStoryById(int storyId) {
+//		return storyRepo.findById(storyId).orElse(null);
+		Story story = null;
+		Optional<Story> storyOpt = storyRepo.findById(storyId);
+		if(storyOpt.isPresent()) {
+			story = storyOpt.get();
+		}
+		return story;
 	}
 
 }
