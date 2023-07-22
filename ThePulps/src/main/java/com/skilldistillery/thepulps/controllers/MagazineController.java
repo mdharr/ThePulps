d32ecaf153fe4d4631a083fb45dba0e29fa515f1
Michael Harrington <mdharr7@gmail.com>
2023-07-22 12:24:32 +0800

    Add rest api endpoint for retrieving magazines by id

--- ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/MagazineController.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/MagazineController.java
@@ -2,9 +2,12 @@ package com.skilldistillery.thepulps.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,14 @@ public class MagazineController {
     @GetMapping("magazines")
     public List<Magazine> getAllMagazines() {
         return magazineService.getAllMagazines();
+    }
+    
+    @GetMapping("magazines/{mid}")
+    public Magazine getMagazineById(@PathVariable("mid") int magazineId, HttpServletResponse res) {
+    	if(magazineService.getMagazineById(magazineId) == null) {
+    		res.setStatus(404);
+    	}
+    	return magazineService.getMagazineById(magazineId);
     }
 
 }
