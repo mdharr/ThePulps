6e807c0932b1dedc79891b990a41edb3f23ad55f
Michael Harrington <mdharr7@gmail.com>
2023-07-23 21:07:39 +0800

    Stub out entity class for Comment and add to class declarations in persistence.xml file to make hibernate aware. Update mapping in Post entity to properly reflect relationship to Comment.

--- JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/Comment.java
+++ JPAThePulps/src/main/java/com/skilldistillery/thepulps/entities/Comment.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.thepulps.entities;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,56 +13,64 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Formula;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-public class Post {
+public class Comment {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@JsonBackReference
 	@ManyToOne
-	@JoinColumn(name = "user_id")
-	@MapsId(value = "userId")
-	private User user;
-	
-	private String content;
-	
+	@JoinColumn(name = "post_id")
+	private Post post;
+    
+	@JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String content;
+    
 	@CreationTimestamp
 	@Column(name = "created_at")
 	private LocalDateTime createdAt;
 	
-	@UpdateTimestamp
-	@Column(name = "updated_at")
-	private LocalDateTime updatedAt;
-	
-	@JsonManagedReference
-	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
-	@Formula("(SELECT MAX(c.created_at) FROM comment c WHERE c.post_id = id)")
-	private List<Comment> comments;
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "parent_comment_id")
+	@JsonIgnoreProperties("replies")
+	private Comment parentComment;
 
-	public Post() {
+	@JsonIgnore
+	@OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL)
+	@OrderBy("createdAt ASC")
+	private List<Comment> replies = new ArrayList<>();
+
+	public Comment() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Post(int id, User user, String content, LocalDateTime createdAt, LocalDateTime updatedAt,
-			List<Comment> comments) {
+	public Comment(int id, Post post, User user, String content, LocalDateTime createdAt, Comment parentComment,
+			List<Comment> replies) {
 		super();
 		this.id = id;
+		this.post = post;
 		this.user = user;
 		this.content = content;
 		this.createdAt = createdAt;
-		this.updatedAt = updatedAt;
-		this.comments = comments;
+		this.parentComment = parentComment;
+		this.replies = replies;
 	}
 
 	public int getId() {
@@ -70,6 +79,14 @@ public class Post {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
 	}
 
 	public User getUser() {
@@ -96,20 +113,20 @@ public class Post {
 		this.createdAt = createdAt;
 	}
 
-	public LocalDateTime getUpdatedAt() {
-		return updatedAt;
+	public Comment getParentComment() {
+		return parentComment;
 	}
 
-	public void setUpdatedAt(LocalDateTime updatedAt) {
-		this.updatedAt = updatedAt;
+	public void setParentComment(Comment parentComment) {
+		this.parentComment = parentComment;
 	}
 
-	public List<Comment> getComments() {
-		return comments;
+	public List<Comment> getReplies() {
+		return replies;
 	}
 
-	public void setComments(List<Comment> comments) {
-		this.comments = comments;
+	public void setReplies(List<Comment> replies) {
+		this.replies = replies;
 	}
 
 	@Override
@@ -125,15 +142,15 @@ public class Post {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Post other = (Post) obj;
+		Comment other = (Comment) obj;
 		return id == other.id;
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("Post [id=").append(id).append(", user=").append(user).append(", content=").append(content)
-				.append(", createdAt=").append(createdAt).append(", updatedAt=").append(updatedAt).append("]");
+		builder.append("Comment [id=").append(id).append(", post=").append(post).append(", user=").append(user)
+				.append(", content=").append(content).append(", createdAt=").append(createdAt).append("]");
 		return builder.toString();
 	}
 
