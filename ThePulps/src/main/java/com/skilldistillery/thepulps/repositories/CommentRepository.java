c29c12d825064648c57b5cefd34f2ef40a70fc21
Michael Harrington <mdharr7@gmail.com>
2023-07-24 12:00:11 +0800

    Create methods in Comment repository for retrieving comments in various ways

--- ThePulps/src/main/java/com/skilldistillery/thepulps/repositories/CommentRepository.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/repositories/CommentRepository.java
@@ -1,9 +1,22 @@
 package com.skilldistillery.thepulps.repositories;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.thepulps.entities.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+	
+	Set<Comment> findByUser_Username(String username);
+	
+	Comment findByIdAndUserId(int commentId, int userId);
+	
+	List<Comment> findByPost_Id(int postId);
+	
+	List<Comment> findByUser_Id(int userId);
+	
+//	Post findPostByPostId(int postId);
 
 }
