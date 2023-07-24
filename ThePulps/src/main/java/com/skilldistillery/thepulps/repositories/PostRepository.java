92128176547c302525ca66a90fc2926d22fdd326
Michael Harrington <mdharr7@gmail.com>
2023-07-24 11:58:57 +0800

    Create methods in Post repository for retrieving posts in various ways

--- ThePulps/src/main/java/com/skilldistillery/thepulps/repositories/PostRepository.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/repositories/PostRepository.java
@@ -1,9 +1,20 @@
 package com.skilldistillery.thepulps.repositories;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.thepulps.entities.Post;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+	
+	Set<Post> findByUser_Username(String username);
+	
+	Post findByIdAndUserId(int postId, int userId);
+
+	Post findByUser_UsernameAndId(String username, int postId);
+	
+	List<Post> findByUser_Id(int userId);
 
 }
