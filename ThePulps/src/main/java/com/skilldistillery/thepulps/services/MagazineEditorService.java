f5c6a85195d5995e0f4aa8dee8e79cf832e8f869
Michael Harrington <mdharr7@gmail.com>
2023-07-27 12:29:31 +0800

    Stub out magazine editor service

---
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/MagazineEditorService.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.thepulps.services;
+
+import java.util.List;
+
+import com.skilldistillery.thepulps.entities.Magazine;
+import com.skilldistillery.thepulps.entities.MagazineEditor;
+
+public interface MagazineEditorService {
+	
+	List<MagazineEditor> getAllMagazineEditors();
+	
+	List<Magazine> getAllMagazinesByMagazineEditorId(int magazineEditorId);
+	
+	MagazineEditor getMagazineEditorByMagazineId();
+
+}
---
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/MagazineEditorServiceImpl.java
@@ -0,0 +1,54 @@
+package com.skilldistillery.thepulps.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.thepulps.entities.Magazine;
+import com.skilldistillery.thepulps.entities.MagazineEditor;
+import com.skilldistillery.thepulps.repositories.MagazineEditorRepository;
+import com.skilldistillery.thepulps.repositories.MagazineRepository;
+
+@Service
+public class MagazineEditorServiceImpl implements MagazineEditorService {
+	
+	@Autowired
+	private MagazineEditorRepository magazineEditorRepo;
+	
+	@Autowired
+	private MagazineRepository magazineRepo;
+
+	@Override
+	public List<MagazineEditor> getAllMagazineEditors() {
+		return magazineEditorRepo.findAll();
+	}
+
+	@Override
+	public List<Magazine> getAllMagazinesByMagazineEditorId(int magazineEditorId) {
+		Optional<MagazineEditor> magazineEditorOptional = magazineEditorRepo.findById(magazineEditorId);
+		
+		if(magazineEditorOptional.isPresent()) {
+			MagazineEditor magazineEditor = magazineEditorOptional.get();
+			List<Magazine> magazines = new ArrayList<>();
+			List<Magazine> magazineEditorMagazines = magazineEditor.getMagazines();
+			for(Magazine magazineEditorMagazine : magazineEditorMagazines) {
+				if(magazineEditorMagazine != null) {
+					magazines.add(magazineEditorMagazine);
+				}
+			}
+			return !magazines.isEmpty() ? magazines : null;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public MagazineEditor getMagazineEditorByMagazineId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
