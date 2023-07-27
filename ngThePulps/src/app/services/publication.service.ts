9983ef1bf29a39cf437248b3876111fb3f059fc7
Michael Harrington <mdharr7@gmail.com>
2023-07-27 16:32:29 +0800

    Generate angular frontend services for publication and magazine. Stub out publication service.

---
+++ ngThePulps/src/app/services/publication.service.ts
@@ -0,0 +1,53 @@
+import { DatePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Publication } from '../models/publication';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublicationService {
+  private url = environment.baseUrl + 'api/publications';
+  private otherUrl = environment.baseUrl + 'api';
+
+  constructor(private http: HttpClient, private datePipe: DatePipe, private authService: AuthService) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<Publication[]> {
+    return this.http.get<Publication[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error(
+            'PublicationService.indexAll(): error retrieving publication list: ' + err
+          )
+        );
+      })
+    );
+  }
+
+  find(id: number): Observable<Publication> {
+    return this.http.get<Publication>(`${this.url}/${id}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('PublicationService.find(): error retrieving publication: ' + err)
+        );
+      })
+    );
+  }
+}
