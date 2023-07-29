7816ed24f17da19762cd77837c70726db0643b1f
Michael Harrington <mdharr7@gmail.com>
2023-07-29 12:30:21 +0800

    Stub out Story service in Angular frontend

--- ngThePulps/src/app/models/story.ts
+++ ngThePulps/src/app/models/story.ts
@@ -1,2 +1,37 @@
+import { Magazine } from "./magazine";
+import { MagazineHtml } from "./magazine-html";
+
 export class Story {
+  id: number;
+  title: string;
+  createdAt: string;
+  // storyPdfs: StoryPdf[];
+  // authors: Author[];
+  // tags: Tag[];
+  magazines: Magazine[];
+  // collections: Collection[];
+  // members: Member[];
+  // storyComments: StoryComment[];
+  thumbnailUrl: string;
+  imageUrl: string;
+  htmlMagazines: MagazineHtml[];
+  // storyAnchors: StoryAnchor[];
+
+  constructor(
+    id: number = 0,
+    title: string = '',
+    createdAt: string = '',
+    magazines: Magazine[] = [],
+    thumbnailUrl: string = '',
+    imageUrl: string = '',
+    htmlMagazines: MagazineHtml[] = []
+  ) {
+    this.id = id;
+    this.title = title;
+    this.createdAt = createdAt;
+    this.magazines = magazines;
+    this.thumbnailUrl = thumbnailUrl;
+    this.imageUrl = imageUrl;
+    this.htmlMagazines = htmlMagazines;
+  }
 }
