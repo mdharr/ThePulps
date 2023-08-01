4a8874ebf5b9aecac1ead475ac9e3512ae6efae2
Michael Harrington <mdharr7@gmail.com>
2023-08-01 21:31:40 +0800

    Stub out story image service implementation

---
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/StoryImageServiceImpl.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.thepulps.services;
+
+import java.util.List;
+
+import com.skilldistillery.thepulps.entities.StoryImage;
+
+public class StoryImageServiceImpl implements StoryImageService {
+
+	@Override
+	public List<StoryImage> getAllStoryImages() {
+		return null;
+	}
+
+	@Override
+	public List<StoryImage> getAllStoryImagesByStoryId(int storyId) {
+		return null;
+	}
+
+}
