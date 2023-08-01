d08e6307c6305a647a1440251db03139433577ec
Michael Harrington <mdharr7@gmail.com>
2023-08-01 21:48:22 +0800

    Create rest api endpoints in advertisement controller for retrieving all advertisements and all advertisements by magazine id. Test successfully with Postman.

--- ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/AdvertisementController.java
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/controllers/AdvertisementController.java
@@ -1,10 +1,18 @@
 package com.skilldistillery.thepulps.controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.thepulps.entities.Advertisement;
 import com.skilldistillery.thepulps.services.AdvertisementService;
 
 @RestController
@@ -14,5 +22,14 @@ public class AdvertisementController {
 	
 	@Autowired
 	private AdvertisementService advertisementService;
-
+	
+	@GetMapping("advertisements")
+	public List<Advertisement> getAllAdvertisements() {
+		return advertisementService.getAllAdvertisements();
+	}
+	
+	@GetMapping("magazines/{mid}/advertisements")
+	public List<Advertisement> getAllAdvertisementsByMagazineId(@PathVariable("mid") int magazineId, HttpServletRequest req, HttpServletResponse res) {
+		return advertisementService.getAllAdvertisementsByMagazineId(magazineId);
+	}
 }
