770eafc3f1cbcad9ccc127698b785f85cf730f3f
Michael Harrington <mdharr7@gmail.com>
2023-08-01 21:25:17 +0800

    Stub out advertisement service implementation

---
+++ ThePulps/src/main/java/com/skilldistillery/thepulps/services/AdvertisementServiceImpl.java
@@ -0,0 +1,21 @@
+package com.skilldistillery.thepulps.services;
+
+import java.util.List;
+
+import com.skilldistillery.thepulps.entities.Advertisement;
+
+public class AdvertisementServiceImpl implements AdvertisementService {
+
+	@Override
+	public List<Advertisement> getAllAdvertisements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Advertisement> getAllAdvertisementsByMagazineId(int magazineId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
