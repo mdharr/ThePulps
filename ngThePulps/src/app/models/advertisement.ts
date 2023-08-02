33bf7d3bce7907831f6cae1f42470a6c860c0029
Michael Harrington <mdharr7@gmail.com>
2023-08-02 09:32:20 +0800

    Stub out advertisement and story image models.

--- ngThePulps/src/app/models/advertisement.ts
+++ ngThePulps/src/app/models/advertisement.ts
@@ -1,2 +1,17 @@
+import { Magazine } from "./magazine";
+
 export class Advertisement {
+  id: number;
+  magazine: Magazine;
+  imageUrl: string;
+
+  constructor(
+    id: number = 0,
+    magazine: Magazine = new Magazine(),
+    imageUrl: string = ''
+  ) {
+    this.id = id;
+    this.magazine = magazine;
+    this.imageUrl = imageUrl;
+  }
 }
--- ngThePulps/src/app/models/story-image.ts
+++ ngThePulps/src/app/models/story-image.ts
@@ -1,2 +1,19 @@
+import { Story } from "./story";
+
 export class StoryImage {
+  id: number;
+  story: Story;
+  imageUrl: string;
+  // stories: Story[];
+  // storyAnchors: StoryAnchor[];
+
+  constructor(
+    id: number = 0,
+    story: Story = new Story(),
+    imageUrl: string = ''
+  ) {
+    this.id = id;
+    this.story = story;
+    this.imageUrl = imageUrl;
+  }
 }
