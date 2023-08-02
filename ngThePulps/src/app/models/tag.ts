e62c52d0977bd46cc7eff5ca6be554122d6c7c1a
Michael Harrington <mdharr7@gmail.com>
2023-08-02 10:10:20 +0800

    Stub out Tag model and Genre enum in Angular frontend

--- ngThePulps/src/app/models/tag.ts
+++ ngThePulps/src/app/models/tag.ts
@@ -1,2 +1,40 @@
+import { Magazine } from "./magazine";
+import { Publication } from "./publication";
+import { Story } from "./story";
+
 export class Tag {
+  id: number;
+  genres: Genre[];
+  // authors: Author[];
+  stories: Story[];
+  magazines: Magazine[];
+  publications: Publication[];
+
+  constructor(
+    id: number = 0,
+    genres: Genre[] = [],
+    stories: Story[] = [],
+    magazines: Magazine[] = [],
+    publications: Publication[] = []
+
+  ) {
+    this.id = id;
+    this.genres = genres;
+    this.stories = stories;
+    this.magazines = magazines;
+    this.publications = publications;
+  }
+}
+
+export enum Genre {
+  DETECTIVE_CRIME,
+  HORROR,
+  WESTERN,
+  ADVENTURE,
+  ROMANCE,
+  SCIENCE_FICTION,
+  FANTASY,
+  WAR_AND_AIR_COMBAT,
+  SPORTS,
+  NOT_SPECIFIED
 }
