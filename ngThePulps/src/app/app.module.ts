9728434cbfe629f9df0baeb8a32db5c77cf6b23b
Michael Harrington <mdharr7@gmail.com>
2023-08-08 11:30:46 +0800

    Implement quick jump mat menu for navigating to various components from a dropdown menu in the navbar.

--- ngThePulps/src/app/app.module.ts
+++ ngThePulps/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ngThePulps/src/app/components/navbar/navbar.component.ts
+++ ngThePulps/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  title: string = "Dashboard";
+
+  loggedInUser: User = new User();
+
+  isRotated1: boolean = false;
+
+  quickJumpText: string = 'Browse';
+
+  private loggedInUserSubscription: Subscription | undefined;
+
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
+
+    rotateChevronTop() {
+      this.isRotated1 = !this.isRotated1;
+
+    }
+
+    resetRotationState() {
+      console.log('reset rotation state function called');
+
+      this.isRotated1 = false;
+    }
+
+    menuOpen(){
+      console.log("open")
+    }
 
 }
