dd73f041ad1dbf368263f79b0665b2094dc6c450
Michael Harrington <mdharr7@gmail.com>
2023-08-20 23:02:12 +0800

    Generate author profile model in angular frontend.

---
+++ ngThePulps/src/app/models/author-profile.spec.ts
@@ -0,0 +1,7 @@
+import { AuthorProfile } from './author-profile';
+
+describe('AuthorProfile', () => {
+  it('should create an instance', () => {
+    expect(new AuthorProfile()).toBeTruthy();
+  });
+});
