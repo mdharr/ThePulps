65f4547807a53c90c18c335340ad94ffda107e7b
Michael Harrington <mdharr7@gmail.com>
2023-08-23 11:05:56 +0800

    Stub out artist model in angular frontend.

--- ngThePulps/src/app/models/artist.ts
+++ ngThePulps/src/app/models/artist.ts
@@ -1,2 +1,20 @@
 export class Artist {
+
+  id: number;
+  fullName: string;
+
+  thumbnailUrl: string;
+  imageUrl: string;
+
+  constructor(
+    id: number = 0,
+    fullName: string = '',
+    thumbnailUrl: string = '',
+    imageUrl: string = ''
+  ) {
+    this.id = id;
+    this.fullName = fullName;
+    this.thumbnailUrl = thumbnailUrl;
+    this.imageUrl = imageUrl;
+  }
 }
