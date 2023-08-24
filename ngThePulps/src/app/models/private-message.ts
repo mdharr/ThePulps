52ca01c3cb9a5a11fdf8bedf3c623231aa94217d
Michael Harrington <mdharr7@gmail.com>
2023-08-24 21:51:09 +0800

    Complete private message model class in angular frontend.

--- ngThePulps/src/app/models/private-message.ts
+++ ngThePulps/src/app/models/private-message.ts
@@ -1,2 +1,28 @@
+import { User } from "./user";
+import { UserConversation } from "./user-conversation";
+
 export class PrivateMessage {
+
+  id: number;
+  content: string;
+  createdAt: string;
+  userConversation: UserConversation;
+  sender: User;
+  recipient: User;
+
+  constructor(
+    id: number = 0,
+    content: string = '',
+    createdAt: string = '',
+    userConversation: UserConversation = new UserConversation(),
+    sender: User = new User(),
+    recipient: User = new User()
+  ) {
+    this.id = id;
+    this.content = content;
+    this.createdAt = createdAt;
+    this.userConversation = userConversation;
+    this.sender = sender;
+    this.recipient = recipient;
+  }
 }
