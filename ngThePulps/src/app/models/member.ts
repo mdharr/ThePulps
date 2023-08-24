693bef15870dc698d42acd230ef880c68ceddceb
Michael Harrington <mdharr7@gmail.com>
2023-08-24 21:46:51 +0800

    Complete member model class in angular frontend.

--- ngThePulps/src/app/models/member.ts
+++ ngThePulps/src/app/models/member.ts
@@ -1,2 +1,22 @@
 export class Member {
+
+  id: number;
+  name: string;
+  description: string;
+  imageUrl: string;
+  thumbnailUrl: string;
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    description: string = '',
+    imageUrl: string = '',
+    thumbnailUrl: string = ''
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.thumbnailUrl = thumbnailUrl;
+  }
 }
