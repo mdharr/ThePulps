1a4fddff818f68ed23a05221da4d2cb8b0d3f57c
Michael Harrington <mdharr7@gmail.com>
2023-08-24 21:55:40 +0800

    Complete user profile model class in angular frontend.

--- ngThePulps/src/app/models/user-profile.ts
+++ ngThePulps/src/app/models/user-profile.ts
@@ -1,2 +1,16 @@
 export class UserProfile {
+
+  id: number;
+  imageUrl: string;
+  bio: string;
+
+  constructor(
+    id: number = 0,
+    imageUrl: string = '',
+    bio: string = ''
+  ) {
+    this.id = id;
+    this.imageUrl = imageUrl;
+    this.bio = bio;
+  }
 }
