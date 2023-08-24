9f17edc9f25d7175143138576f6b75f20beb7278
Michael Harrington <mdharr7@gmail.com>
2023-08-24 21:44:47 +0800

    Complete collection model class in angular frontend.

--- ngThePulps/src/app/models/collection.ts
+++ ngThePulps/src/app/models/collection.ts
@@ -1,2 +1,21 @@
 export class Collection {
+  id: number;
+  name: string;
+  createdAt: string;
+  thumbnailUrl: string;
+  backgroundUrl: string;
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    createdAt: string = '',
+    thumbnailUrl: string = '',
+    backgroundUrl: string = ''
+  ) {
+    this.id = id;
+    this.name = name;
+    this.createdAt = createdAt;
+    this.thumbnailUrl = thumbnailUrl;
+    this.backgroundUrl = backgroundUrl;
+  }
 }
