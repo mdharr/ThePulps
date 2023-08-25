b6dd6cfe2adb931879f96cd687f544f33612fb48
Michael Harrington <mdharr7@gmail.com>
2023-08-25 21:29:53 +0800

    Add getHttpOptions, indexAll, and find methods in frontend user service.

--- ngThePulps/src/app/services/user.service.ts
+++ ngThePulps/src/app/services/user.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private url = environment.baseUrl + 'api/users';
+  private otherUrl = environment.baseUrl + 'api';
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error(
+            'MagazineService.indexAll(): error retrieving magazine list: ' + err
+          )
+        );
+      })
+    );
+  }
+
+  find(id: number): Observable<User> {
+    return this.http.get<User>(`${this.url}/${id}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('MagazineService.find(): error retrieving magazine: ' + err)
+        );
+      })
+    );
+  }
 }
