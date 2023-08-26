1609743dd2c9e648957d33090cc303f298ddc4de
Michael Harrington <mdharr7@gmail.com>
2023-08-26 16:21:06 +0800

    Implement ngOnDestroy lifecycle hook to properly destroy subscription in authors component

--- ngThePulps/src/app/components/authors/authors.component.ts
+++ ngThePulps/src/app/components/authors/authors.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Author } from 'src/app/models/author';
 import { AuthorService } from 'src/app/services/author.service';
@@ -8,7 +8,7 @@ import { AuthorService } from 'src/app/services/author.service';
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
-export class AuthorsComponent implements OnInit {
+export class AuthorsComponent implements OnInit, OnDestroy {
   authors: Author[] = [];
 
   private authorsSubscription: Subscription | undefined;
@@ -26,5 +26,11 @@ export class AuthorsComponent implements OnInit {
         console.error(fail);
       }
     });
+  }
+
+  ngOnDestroy() {
+    if (this.authorsSubscription) {
+      this.authorsSubscription.unsubscribe();
+    }
   }
 }
