2f38307dd740607c35bf92628e93092f5bc81830
Michael Harrington <mdharr7@gmail.com>
2023-08-26 13:01:15 +0800

    Subscribe to magazine service to index all magazines in magazines-all component

--- ngThePulps/src/app/components/magazines-all/magazines-all.component.ts
+++ ngThePulps/src/app/components/magazines-all/magazines-all.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Magazine } from 'src/app/models/magazine';
+import { MagazineService } from 'src/app/services/magazine.service';
 
 @Component({
   selector: 'app-magazines-all',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./magazines-all.component.css']
 })
 export class MagazinesAllComponent {
+  magazines: Magazine[] = [];
 
+  private magazinesSubscription: Subscription | undefined;
+
+  magazinesService = inject(MagazineService);
+
+  ngOnInit(): void {
+
+    this.magazinesSubscription = this.magazinesService.indexAll().subscribe({
+      next: (magazines) => {
+        this.magazines = magazines;
+      },
+      error: (fail) => {
+        console.error('Error getting magazines');
+        console.error(fail);
+      }
+    });
+  }
 }
