4ded343456376744ae0de0b8dcfd90045a815c15
Michael Harrington <mdharr7@gmail.com>
2023-08-26 16:25:22 +0800

    Implement on init and on destroy lifecycle hooks in user collections component

--- ngThePulps/src/app/components/user-collections/user-collections.component.ts
+++ ngThePulps/src/app/components/user-collections/user-collections.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-collections',
   templateUrl: './user-collections.component.html',
   styleUrls: ['./user-collections.component.css']
 })
-export class UserCollectionsComponent {
+export class UserCollectionsComponent implements OnInit, OnDestroy {
+
+  user: User = new User();
+
+  ngOnInit(): void {
+
+  }
+  ngOnDestroy(): void {
+
+  }
 
 }
