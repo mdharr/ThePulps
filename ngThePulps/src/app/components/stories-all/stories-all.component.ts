d9bdcb141dbc9cb7587759be6a03611c872bf95b
Michael Harrington <mdharr7@gmail.com>
2023-08-26 13:05:39 +0800

    Subscribe to magazine service to index all stories in stories-all component

--- ngThePulps/src/app/components/stories-all/stories-all.component.ts
+++ ngThePulps/src/app/components/stories-all/stories-all.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Story } from 'src/app/models/story';
+import { StoryService } from 'src/app/services/story.service';
 
 @Component({
   selector: 'app-stories-all',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./stories-all.component.css']
 })
 export class StoriesAllComponent {
+  stories: Story[] = [];
 
+  private storiesSubscription: Subscription | undefined;
+
+  storyService = inject(StoryService);
+
+  ngOnInit(): void {
+
+    this.storiesSubscription = this.storyService.indexAll().subscribe({
+      next: (stories) => {
+        this.stories = stories;
+      },
+      error: (fail) => {
+        console.error('Error getting stories');
+        console.error(fail);
+      }
+    });
+  }
 }
