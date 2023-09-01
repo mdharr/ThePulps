44d28bfe99f50c15f1550cf5d1644b8b9e5ac7c0
Michael Harrington <mdharr7@gmail.com>
2023-09-01 19:50:16 +0800

    Update frontend user profile model to reflect newly implemented column.

--- ngThePulps/src/app/models/user-profile.ts
+++ ngThePulps/src/app/models/user-profile.ts
@@ -1,16 +1,24 @@
+import { User } from "./user";
+
 export class UserProfile {
 
   id: number;
   imageUrl: string;
   bio: string;
+  bannerUrl: string;
+  user: User;
 
   constructor(
     id: number = 0,
     imageUrl: string = '',
-    bio: string = ''
+    bio: string = '',
+    bannerUrl: string = '',
+    user: User = new User()
   ) {
     this.id = id;
     this.imageUrl = imageUrl;
     this.bio = bio;
+    this.bannerUrl = bannerUrl;
+    this.user = user;
   }
 }
